e4fa676f10f6c3b022219ab3a048762ce03b0a47
Alec Slaney <alecg.sla@gmail.com>
2020-03-30 00:28:29 -0700

    Added date filter code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,32 @@
-// from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// References to HTML elements for changing page
+var tbody = d3.select("tbody");
+var input = d3.select("#datetime");
+var filterButton = d3.select("#filter-btn");
+
+// Constructs table on page based on filter criteria
+function buildTable(filter) {
+  filter.forEach((ufoReport) => {
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+};
+
+// Event handler for "Filter" button. Filters data based on user-input date
+filterButton.on("click", () => {
+  var userDate = input.property("value");
+  tbody.html("");
+  if (userDate) {
+    var filtered = tableData.filter(x => x.datetime === userDate);
+    buildTable(filtered);
+  } else {
+    buildTable(data);
+  }
+});
+
+// On page load, constructs table in its entirety
+buildTable(data);
